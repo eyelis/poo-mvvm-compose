6fcb7d496e8ae0fbd86abaa09a47b9edcb24326a
basil <basileye@gmail.com>
2024-08-14 09:29:58 +0200

    refacto

--- app/src/test/java/com/openclassrooms/notes/service/LocalNotesApiServiceTest.kt
+++ app/src/test/java/com/openclassrooms/notes/service/LocalNotesApiServiceTest.kt
@@ -1,8 +1,10 @@
 package com.openclassrooms.notes.service
 
 import com.openclassrooms.notes.model.Note
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -12,24 +14,69 @@ class LocalNotesApiServiceTest {
     @Test
     fun getNotes() = runTest {
         /* Given */
-        val expectedNotes = flow {
+
+        val expectedNotes = mutableListOf(
+            Note(1, "title1", "body1"),
+            Note(2, "title2", "body3"),
+            Note(3, "title3", "body3")
+        )
+
+        val expectedNotesFlow = flow {
             emit(
-                mutableListOf(
-                    Note(1, "title1", "body1"),
-                    Note(2, "title2", "body2"),
-                    Note(3, "title3", "body3")
-                )
+                expectedNotes
             )
         }
 
-        val service = LocalNotesApiService(expectedNotes.first())
+        val service = LocalNotesApiService(expectedNotes)
 
         /* When */
-        val firstNote = service.getAllNotes().first()
+        val foundNotesFlow = service.getAllNotes()
 
         /* Then */
-        assertEquals(expectedNotes.first(), firstNote)
+        assertEquals(expectedNotesFlow.count(), foundNotesFlow.count())
+        assertEquals(expectedNotesFlow.first(), foundNotesFlow.first())
+        assertEquals(expectedNotesFlow.toList(), foundNotesFlow.toList())
     }
+
+    @Test
+    fun getNotesFirst() = runTest {
+        /* Given */
+
+        val expectedNotes = mutableListOf(
+            Note(1, "title1", "body1"),
+            Note(2, "title2", "body3"),
+            Note(3, "title3", "body3")
+        )
+
+        val expectedNotesFlow = flow {
+            emit(
+                mutableListOf(
+                    expectedNotes[0]
+                )
+            )
+            //delay(1000)
+            emit(
+                mutableListOf(
+                    expectedNotes[1]
+                )
+            )
+            //delay(1000)
+            emit(
+                mutableListOf(
+                    expectedNotes[2]
+                )
+            )
+        }
+        val service = LocalNotesApiService(expectedNotesFlow.first())
+
+        /* When */
+        val foundNotesFlow = service.getAllNotes()
+
+        /* Then */
+        assertEquals(expectedNotesFlow.first(), foundNotesFlow.first())
+
+    }
+
 
     @Test
     fun addNotes() = runTest {
